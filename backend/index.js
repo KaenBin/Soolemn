2e1a99f1fc0a380cee3bea2742798f3b732f4f70
phuc131102 <phuc32969@gmail.com>
2024-04-22 22:14:12 +0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,15 @@ const stripe = Stripe(
   "sk_test_51P4emDIcJNDJCIe2S5d5KZViJAHfWV45vjCZ4VloaX7jH6ektWN6UaG0lt6W5sNa0c22FqNjwtCch2z9yzmNB9Ko00DyF4CpuJ"
 );
 
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://soolemn-ui-2.onrender.com"],
+};
+
 // app.use("/", authMiddleware);
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 
